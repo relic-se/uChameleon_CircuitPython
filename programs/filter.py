77055c20510b50d8c11b571bfed848fc15a10269
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-08-19 09:38:49 -0500

    Add variable gain to filter

--- programs/filter.py
+++ programs/filter.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPLv3
 
-from audiofilters import Filter
+from audiofilters import Distortion, Filter
 import synthio
 
 from uchameleon import uChameleon
@@ -22,6 +22,9 @@ MAX_DEPTH   = (MAX_FILTER - MIN_FILTER) / 4  # hz
 
 MIN_POLES   = 1
 MAX_POLES   = 2
+
+MIN_GAIN    = 10.0  # dB
+MAX_GAIN    = 20.0  # dB
 
 BUFFER_SIZE = 2048  # bytes
 
@@ -55,17 +58,25 @@ filters = [
     for i in range(MAX_POLES)
 ]
 
-effect = Filter(
-    filter=tuple(filters[:MIN_POLES]),
+effect_filter = Filter(
+    filter=tuple(filters[:MIN_POLES if pedal.left_switch.value else MAX_POLES]),
 
     buffer_size=BUFFER_SIZE,
     **pedal.audiosample_args,
 )
 
+effect_gain = Distortion(
+    soft_clip=True,
+    mix=0.0,
+    **pedal.audiosample_args,
+)
+
 # Audio Chain
 pedal.play(
-    effect.play(
-        pedal.audio_in
+    effect_gain.play(
+        effect_filter.play(
+            pedal.audio_in
+        )
     )
 )
 
@@ -79,13 +90,13 @@ while True:
     q = pow(pots[1], 2) * (MAX_Q - MIN_Q) + MIN_Q
     for x in filters:
         x.Q = q
+    effect_gain.pre_gain = pots[1] * (MAX_GAIN - MIN_GAIN) + MIN_GAIN
     lfo.rate = (pow(pots[2], 2) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED) * (1 + (not pedal.left_switch.value and auto))
 
-    lfo.scale = (not pedal.left_switch.value or auto)
-
-    poles = MIN_POLES if pedal.right_switch.value else MAX_POLES
-    if len(effect.filter) != poles:
-        effect.filter = tuple(filters[:poles])
+    lfo.scale = (not pedal.right_switch.value or auto)
+    
+    if pedal.left_switch.rose or pedal.left_switch.fell:
+        effect_filter.filter = tuple(filters[:MIN_POLES if pedal.left_switch.value else MAX_POLES])
 
     if pedal.left_button.pressed:
         auto = True
@@ -95,6 +106,7 @@ while True:
     if pedal.right_button.released:
         pedal.bypass = not pedal.bypass
         pedal.mix = not pedal.bypass
-        effect.mix = not pedal.bypass
+        effect_filter.mix = not pedal.bypass
+        effect_gain.mix = not pedal.bypass
 
     pedal.led = (lfo.value + 1) / 2 * (not pedal.bypass)
